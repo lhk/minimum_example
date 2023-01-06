feed88311661c71e980d4ad0260f1e7cc25e2d70
Lars Klein <lars.klein@epfl.ch>
2023-01-06 10:56:45 +0100

    adding example code

---
+++ src/components/Problem.svelte
@@ -0,0 +1,7 @@
+<p class="bg-blue-700">Using Tailwind class</p>
+
+<style lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Problem from "./../components/Problem.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Problem></Problem>
